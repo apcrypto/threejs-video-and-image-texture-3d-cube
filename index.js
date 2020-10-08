52fdb3260f558a2de5488aafe67ea99f5318fb6a
Andy Purbrick <andypurbrick@gmail.com>
2020-10-08 14:31:00 +0100

    added comments

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 var scene, camera, renderer, loader, geometry, light;
 // custom global variables
 var materials, video, videoImage, videoImageContext, videoTexture, cube;
-
+// setup scene, camera and renderer
 scene = new THREE.Scene();
 camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
+// create video element and set attributes so that video autoplays on iOS and Android
 video = document.createElement("video");
 video.setAttribute("muted", "");
 video.setAttribute("playsinline", "");
@@ -17,20 +17,20 @@ video.autoplay = true;
 video.loop = true;
 video.muted = true;
 video.src = "textures/video.mp4";
-
+// create canvas element for video
 videoImage = document.createElement("canvas");
-
+// define context for canvas
 videoImageContext = videoImage.getContext("2d");
 videoImageContext.fillStyle = "#000000";
 videoImageContext.fillRect(0, 0, videoImage.width, videoImage.height);
-
+// create new video texture using video element
 videoTexture = new THREE.VideoTexture(video);
 videoTexture.minFilter = THREE.LinearFilter;
 videoTexture.magFilter = THREE.LinearFilter;
 videoTexture.format = THREE.RGBFormat;
-
+// create new TextureLoader
 loader = new THREE.TextureLoader();
-
+// create materials array
 materials = [
   new THREE.MeshBasicMaterial({
     map: loader.load("textures/TexturesCom_Camouflage0018_seamless_S.jpg"),
@@ -55,16 +55,18 @@ materials = [
     map: loader.load("textures/TexturesCom_WoodChips0040_1_seamless_S.jpg"),
   }),
 ];
-
+// create cube mesh using geoetry and materials
 geometry = new THREE.BoxGeometry(2.2, 3.2, 2.2);
 cube = new THREE.Mesh(geometry, materials);
 scene.add(cube);
+// set camera position
+camera.position.z = 5;
 
 function onUpdate() {
   cube.position.x = position.x;
   cube.position.y = position.y;
 }
-
+// on update keep track of cube position
 var position = { x: 0.0, y: 0.0 };
 
 var tweenToPosOne = new TWEEN.Tween(cube.rotation)
@@ -90,8 +92,6 @@ tweenToPosThree.chain(tweenToPosFour);
 tweenToPosFour.chain(tweenToPosOne);
 
 tweenToPosOne.start();
-
-camera.position.z = 5;
 
 light = new THREE.AmbientLight("rgb(255,255,255)"); // soft white light
 scene.add(light);
